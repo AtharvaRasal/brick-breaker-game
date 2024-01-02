0de3b32f772bd972d81d3be011aa19550cb9a1a3
Atharva_Rasal <Atharva.rasal2000@gmail.com>
2024-01-02 22:50:29 +0530

    Add files via upload

---
+++ src/brickBracker/MapGenerator.java
@@ -0,0 +1,36 @@
+package brickBracker;
+
+import java.awt.*;
+
+public class MapGenerator {
+    public int map[][];
+    public int brickWidth;
+    public int brickHight;
+    public MapGenerator(int row,int col){
+        map = new int[row][col];
+        for (int i=0; i< map.length;i++){
+            for (int j=0;j< map[0].length;j++){
+                map[i][j] = 1;
+            }
+        }
+        brickWidth = 540/col;
+        brickHight = 150/row;
+    }
+    public void draw(Graphics2D g){
+        for (int i=0; i< map.length;i++){
+            for (int j=0;j< map[0].length;j++){
+                if (map[i][j] > 0) {        // checks if the value of the current brick is greater than 0, indicating that the brick is still present
+                    g.setColor(Color.white);
+                    g.fillRect(j * brickWidth + 80, i * brickHight + 50, brickWidth, brickHight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j * brickWidth + 80, i * brickHight + 50, brickWidth, brickHight);
+                }
+            }
+        }
+    }
+    public void setBrickValue(int value,int row,int col){
+        map[row][col] = value;
+    }
+}
